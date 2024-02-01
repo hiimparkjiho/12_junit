c3a258520a8cc2b78850343444c26dcab861505b
hiimparkjiho <jiho2689@gmail.com>
2024-02-01 18:30:31 +0900

    junit

---
+++ chap03_parameterized-test/src/main/java/com/ohgiraffers/parameterized/section01/params/Calculator.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.parameterized.section01.params;
+
+public class Calculator {
+    public static int sumTowNumbers(int num1, int num2){
+
+        return num1 + num2;
+    }
+}
